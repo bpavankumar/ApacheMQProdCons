ccf703647a98baeb4ff1a01dcd737c0917c77ee4
bpavankumar <pavan.b9@gmail.com>
2016-07-06 14:40:35 +0530

    Add files via upload

---
+++ Consumer.java
@@ -0,0 +1,47 @@
+package com.apachemq.example;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer {
+	private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
+
+	// Name of the queue we will receive messages from
+	private static String subject = "ApacheMQExample";
+
+	public static void main(String[] args) throws JMSException {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue(subject);
+
+		// MessageConsumer is used for receiving (consuming) messages
+		MessageConsumer consumer = session.createConsumer(destination);
+
+		// Here we receive the message.
+		// By default this call is blocking, which means it will wait
+		// for a message to arrive on the queue.
+		Message message = consumer.receive();
+
+		// There are many types of Message and TextMessage
+		// is just one of them. Producer sent us a TextMessage
+		// so we must cast to it to get access to its .getText()
+		// method.
+		if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			System.out.println("Received: '" + textMessage.getText() + "'");
+		}
+		connection.close();
+	}
+}
